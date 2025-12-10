118182d616166b8ff70d0fefc4a871e52689914b
Erik <erikmuar91@gmail.com>
2025-12-10 17:59:49 +0100

    microbit-emisor

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hola microbit A!")
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("Hola microbit B!")
+})
+radio.setGroup(1)
