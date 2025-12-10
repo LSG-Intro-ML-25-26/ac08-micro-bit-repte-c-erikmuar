40170e0f74592a0914f4ace3325c14e22bf34204
Erik <erikmuar91@gmail.com>
2025-12-10 19:57:45 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hola microbit A!")
+    basic.showString("Enviat!")
+})
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
-    basic.pause(100)
-    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+    basic.showString("Enviat!")
 })
 radio.setGroup(1)
